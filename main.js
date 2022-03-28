22cf602fc16f0d9fd256f4aa72470074e62d0bff
Luis Castro Martín <luiscastro193@gmail.com>
2022-03-28 23:48:08 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,16 +2,19 @@
 const root = document.querySelector(':root');
 const buttonContainer = document.querySelector('.full-size');
 let enabled = false;
+let initTime;
 
 document.addEventListener('click', function() {
 	if (!enabled) {
 		root.style.setProperty('--angle', `${Math.random()}turn`);
 		buttonContainer.classList.remove("hidden");
 		enabled = true;
+		initTime = Date.now();
 	}
 }, true);
 
 document.querySelector('.button').addEventListener('click', function() {
+	alert(`${Date.now() - initTime}ms`);
 	buttonContainer.classList.add("hidden");
 	enabled = false;
 });
