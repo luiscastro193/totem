f256a55f12daccaee94a6213651ffbf8d27e29f2
Luis <luiscastro193@gmail.com>
2022-07-21 19:56:37 +0200

    Trunc > floor

--- main.js
+++ main.js
@@ -139,7 +139,7 @@ function setPush(time) {
 function calculateWinner() {
 	let minTime = Math.min(...pushTimes.values());
 	let winners = [...pushTimes.keys()].filter(player => pushTimes.get(player) == minTime);
-	return winners[Math.floor(Math.random() * winners.length)];
+	return winners[Math.trunc(Math.random() * winners.length)];
 }
 
 function setWinner(player) {
@@ -206,7 +206,7 @@ async function setAsHost() {
 	await setNPlayers();
 	await setName();
 	
-	const code = Math.floor(Math.random() * 10000).toString().padStart(4, '0');
+	const code = Math.trunc(Math.random() * 10000).toString().padStart(4, '0');
 	info.textContent = `Hosting room ${code}`;
 	updatePlayers();
 	
